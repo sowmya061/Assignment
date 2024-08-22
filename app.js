d582e4c2efde0e6e2db62ff051a07c3ff110b266
Sowmya Emani <sowmyaemani06@gmail.com>
2024-08-22 20:31:31 +0530

    Initial commit

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandlers');
-
+const ratelimiter=require('./middlewares/ratelimiter');
 const app = express();
-
 app.use(bodyParser.json());
 app.use('/api', routes);
 app.use(errorHandler);
+app.use(ratelimiter);
+
 
 module.exports = app;
